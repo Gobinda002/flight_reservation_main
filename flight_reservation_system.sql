5debf39d32545e503d91c811cc7771ec23540d9a
Gobinda002 <gitesholi222@gmail.com>
2025-08-30 17:31:12 +0545

    asd

--- flight_reservation_system.sql
+++ flight_reservation_system.sql
@@ -1,21 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 24, 2025 at 09:14 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `flight_reservation_system`
@@ -52,19 +40,6 @@ CREATE TABLE `airlines` (
   `added_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `airlines`
---
-
-INSERT INTO `airlines` (`airline_id`, `airline_name`, `added_by`) VALUES
-(12, 'Yeti Air', 6),
-(13, 'buddha Air', 6),
-(14, 'himalayan air', 6),
-(15, 'saurya air', 6);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `bookings`
 --
 
@@ -80,25 +55,8 @@ CREATE TABLE `bookings` (
   `booking_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `bookings`
---
-
-INSERT INTO `bookings` (`booking_id`, `user_id`, `flight_id`, `passenger_name`, `passenger_email`, `passenger_phone`, `nofpassenger`, `seatnumber`, `booking_date`) VALUES
-(6, 1, 25, 'Apple', 'asd@gmail.com', '1232342342', 12, 0, '2025-08-21 21:32:49'),
-(7, 1, 25, 'Apple', 'asd@gmail.com', '1232342342', 12, 1, '2025-08-21 21:33:07'),
-(8, 1, 25, 'Apple', 'asd@gmail.com', '1232342342', 12, 2, '2025-08-21 21:33:25'),
-(9, 1, 25, 'Apple', 'asd@gmail.com', '3242342342', 1, 3, '2025-08-21 21:34:10'),
-(10, 1, 25, 'Apple', 'asd@gmail.com', '3242342342', 1, 4, '2025-08-21 21:34:51'),
-(11, 1, 25, 'Apple', 'sdf@gmail.com', '3453453423', 1, 5, '2025-08-21 21:57:12'),
-(12, 1, 25, 'Apple', 'sdf@gmail.com', '3453453423', 1, 6, '2025-08-21 21:57:48'),
-(13, 1, 25, 'Apple', 'sdf@gmail.com', '3453453423', 1, 7, '2025-08-21 21:57:58');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `flights`
---
+
 
 CREATE TABLE `flights` (
   `flight_id` int(11) NOT NULL,
@@ -114,17 +72,6 @@ CREATE TABLE `flights` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `flights`
---
-
-INSERT INTO `flights` (`flight_id`, `airline_id`, `plane_id`, `origin`, `destination`, `departure_time`, `arrival_time`, `total_seats`, `price`, `booked_seats`) VALUES
-(25, 15, 21, 'pokhara', 'kathmandu', '2025-08-23 09:49:00', '2025-08-23 09:51:00', 20, 1000, 4),
-(26, 14, 19, 'kathmandu', 'pokhara', '2025-08-26 21:46:00', '2025-08-26 21:49:00', 30, 2000, 0),
-(27, 14, 19, 'ktm', 'pkghr', '2025-08-24 21:59:00', '2025-08-24 22:59:00', 30, 30003, 0);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `planes`
 --
 
@@ -136,31 +83,6 @@ CREATE TABLE `planes` (
   `added_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `planes`
---
-
-INSERT INTO `planes` (`plane_id`, `airline_id`, `plane_number`, `capacity`, `added_by`) VALUES
-(1, 10, 'sdaf', 34, 6),
-(2, 2, 'asddrf', 234, 6),
-(3, 2, 'f3223', 234, 6),
-(4, 11, 'asdasd', 32, 6),
-(7, 4, 'asdfaw', 323, 6),
-(8, 4, 'asfq32', 32, 6),
-(14, 2, 'f4eewf', 234, 6),
-(15, 2, '23rasd', 32, 6),
-(17, 13, 'ba234e', 30, 6),
-(18, 14, 'ha2212', 20, 6),
-(19, 14, 'ha3d32', 50, 6),
-(20, 14, 'ha3sdf', 44, 6),
-(21, 15, 'sa2342', 23, 6),
-(22, 15, 'sa34f2', 33, 6),
-(23, 12, 'ya2342', 20, 6),
-(24, 12, 'ya43df', 332, 6);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
